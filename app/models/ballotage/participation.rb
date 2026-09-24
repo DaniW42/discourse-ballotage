f478c568be202f6856bb1296f7bdce7bdd249f95
DaniW42 <21097466+DaniW42@users.noreply.github.com>
2026-09-24 13:40:17 +0200

    Add model schema annotations

--- app/models/ballotage/participation.rb
+++ app/models/ballotage/participation.rb
@@ -9,3 +9,16 @@ module Ballotage
     belongs_to :user
   end
 end
+
+# == Schema Information
+#
+# Table name: ballotage_participations
+#
+#  id        :bigint           not null, primary key
+#  ballot_id :integer          not null
+#  user_id   :integer          not null
+#
+# Indexes
+#
+#  index_ballotage_participations_on_ballot_id_and_user_id  (ballot_id,user_id) UNIQUE
+#
